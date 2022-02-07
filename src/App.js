4d7f609a23079b631419bb5da77ab1a0f4435dd3
Wingky Faisal <wingky.faisal@outlook.com>
2022-02-07 13:44:20 +0100

    adding query in app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
+import React, { useState, useEffect } from "react";
+
+// import API from Amplify library
+import { API } from "aws-amplify";
+
+// import query definition
+import { listPosts } from "./graphql/queries";
+
 import "./App.css";
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+  async function fetchPosts() {
+    try {
+      const postData = await API.graphql({ query: listPosts });
+      setPosts(postData.data.listPosts.items);
+    } catch (err) {
+      console.log({ err });
+    }
+  }
   return (
     <div className="App">
       <h2>Postagram Studio</h2>
+      {posts.map((post) => (
+        <div key={post.id}>
+          <h3>{post.name}</h3>
+          <p>{post.location}</p>
+        </div>
+      ))}
     </div>
   );
 }
